d093ec882f97f43150a569ee7d931d11b3564bf5
j0bIT <jonaspp471@gmail.com>
2021-02-20 16:29:37 +0100

    changes index.php

---
+++ src/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schülerfirma Art und Weise</title>
+    <link rel="stylesheet" href="../dist/css/index.css">
+</head>
+
+<body>
+    <header>
+
+    <div id="header_image">
+        <h3 id="header_title"></h3>
+        <h1 id="header_title"></h1>
+    </div>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+
+                <a class="navbar-brand">Art & Weise</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse bg-dark" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link">Start</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Produkte</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Hintergrund</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Jetzt Bestellen</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Über uns</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+    </header>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</html>
