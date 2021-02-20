5e06c829dab2d9f89c1050b4c7ac5b71f08e8df5
j0bIT <jonaspp471@gmail.com>
2021-02-20 22:10:46 +0100

    add index.js

--- src/index.php
+++ src/index.php
@@ -65,5 +65,5 @@
 </body>
 <script src="../dist/js/jquery.min.js"></script>
 <script src="../dist/js/bootstrap.bundle.min.js"></script>
-<script src="../dist/js/index.js"></script>
+<script src="js/index.js"></script>
 </html>
