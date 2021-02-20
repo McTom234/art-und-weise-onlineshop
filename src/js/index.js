81ed05a648a23c2a1f70acb7729bce829a478c42
j0bIT <jonaspp471@gmail.com>
2021-02-20 22:11:40 +0100

    add index.js

---
+++ src/js/index.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    onstart = true;
+    isScroll = false;
+
+    $(window).scroll(function(e) {
+        if ($(this).scrollTop() < $(this).height() / 10 * 9) {
+            if (!isScroll && onstart) {
+                isScroll = true;
+
+                $('.navbar').removeClass('fixed-top');
+                $('.navbar_placeholder').hide();
+
+                $('body,html').animate({
+                    scrollTop: $(this).height() / 10 * 9,
+                }, 5);
+
+                setTimeout(function() {
+                    isScroll = false;
+                    onstart = false;
+                }, 600);
+            }
+        }
+        if ($(this).scrollTop() == 0) {
+            onstart = true;
+        }
+
+
+    });
+
+
+
+    $('.button_more').click(function() {
+        if (!isScroll) {
+            isScroll = true;
+            $('body,html').animate({
+                scrollTop: $(window).height() / 10 * 9,
+            }, 5);
+            onstart = false;
+            isScroll = false;
+        }
+
+    });
+
+
+});
