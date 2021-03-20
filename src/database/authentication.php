255e1caeadd5d3b4187e4987c3b8819e91ccff3a
McTom234 <mc_tom234@gmx.de>
2021-03-20 15:40:31 +0100

    - fixed authentication.php and index.php variable $loggedIn - added jquery.cookie.js via npm

--- src/database/authentication.php
+++ src/database/authentication.php
@@ -4,8 +4,6 @@ if (session_status() === 1) {
     session_start();
 }
 if(isset($_SESSION['userid'])) {
-    $pdo = require 'connect.php';
-
     $statement = $pdo->query("SELECT email, forename, surname FROM `User` WHERE user_ID = {$_SESSION['userid']}");
     $loggedIn = $statement->fetch();
 }
