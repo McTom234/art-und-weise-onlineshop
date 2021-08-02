62520f0a411880688c0fb944df62ed39a8a2efa6
McTom234 <mc_tom234@gmx.de>
2021-08-02 11:49:02 +0200

    design footer

--- src/views/layout/footer.php
+++ src/views/layout/footer.php
@@ -1,9 +1,3 @@
 <div class="footer">
-    <hr>
-    <div class="container">
-        <div class="row">
-            <div class="col"></div>
-            <div class="col"><span class="float-end copyright">Schülerfirma Art & Weise &copy; 2021</span></div>
-        </div>
-    </div>
+    <span class="copyright">Schülerfirma Art & Weise &copy; 2021</span>
 </div>
