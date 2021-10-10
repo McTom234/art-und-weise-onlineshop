b55305a0b9feae54d13d88d00b4df3647d63cbd8
Jonas <jonas.bellmann@gmx.de>
2021-10-10 03:29:20 +0200

    add form layout

--- src/views/user/login.php
+++ src/views/user/login.php
@@ -6,21 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/forms.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/user-forms.css"/>
 </head>
 
 <body>
 
-<div class="page_form">
+<?php
+$loggedIn = false;
+$navbar_index = "login";
+require __DIR__ . '/../layout/navbar.php';
+?>
+
+<main>
     <form id="menu" action="?login=1<?php if($buy) echo'&buy=1'?>" method="post">
-        <h1>Login</h1>
-        <label>
-            <input type="email" class="form_input" name="email" placeholder="E-Mail"/>
-        </label>
-        <label>
-            <input type="password" class="form_input" name="password" placeholder="Password"/>
-        </label>
+        <h2>Login</h2>
+        <input id="email" type="email" name="email" placeholder=" "/>
+        <label for="email">E-Mail</label>
+        <input id="password" type="password" name="password" placeholder=" "/>
+        <label for="password">Passwort</label>
         <button type="submit">Log In</button>
+        <a class="link-button" href="/registration">Registrieren</a>
 
         <?php if (isset($message)) { ?>
             <div class="error">
@@ -29,7 +34,10 @@
         <?php } ?>
 
     </form>
-</div>
+</main>
+
+<?php require __DIR__ . '/../layout/footer.php'; ?>
+
 </body>
 
 </html>
--- src/views/user/registration.php
+++ src/views/user/registration.php
@@ -4,43 +4,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrierung</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/forms.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/user-forms.css"/>
 </head>
 <body>
-<div class="page_form">
+
+<?php
+$loggedIn = false;
+$navbar_index = "registration";
+require __DIR__ . '/../layout/navbar.php';
+?>
+
+<main>
     <form id="menu" action="?register=1" method="post">
-        <h1>Registrierung</h1>
+        <h2>Registrierung</h2>
         <?php if (isset($infoMessage)): ?>
-            <p class="infoMessage">
+            <p class="info">
                 <?=$infoMessage?>
             </p>
         <?php endif; ?>
-        <label>
-            <input type="text" class="form_input" name="forename" placeholder="Vorname"/>
-            <input type="text" class="form_input" name="surname" placeholder="Nachname"/>
-        </label>
-        <label>
-            <input type="text" class="form_input" name="street" placeholder="Straße"/>
-            <input type="text" class="form_input" name="number" placeholder="Nummer"/>
-            <input type="text" class="form_input" name="postcode" placeholder="PLZ"/>
-            <input type="text" class="form_input" name="city" placeholder="Stadt"/>
-        </label>
-        <label>
-            <input type="email" class="form_input" name="email" placeholder="E-Mail"/>
-        </label>
-        <label>
-            <input type="password" class="form_input" name="password" placeholder="Passwort"/>
-            <input type="password" class="form_input" name="password2" placeholder="Passwort wiederholen"/>
-        </label>
-        <button type="submit">Log In</button>
+        <fieldset>
+            <div>
+                <input id="forename" type="text" class="form_input" name="forename" placeholder=" "/>
+                <label for="forename">Vorname</label>
+            </div>
+            <div>
+                <input id="surname" type="text" class="form_input" name="surname" placeholder=" "/>
+                <label for="surname">Nachname</label>
+            </div>
+        </fieldset>
+        <fieldset>
+            <div>
+                <input id="street" type="text" class="form_input" name="street" placeholder=" "/>
+                <label for="street">Straße</label>
+            </div>
+            <div>
+                <input id="number" type="text" class="form_input" name="number" placeholder=" "/>
+                <label for="number">Nummer</label>
+            </div>
+            <div>
+                <input id="postcode" type="text" class="form_input" name="postcode" placeholder=" "/>
+                <label for="postcode">PLZ</label>
+            </div>
+            <div>
+                <input id="city" type="text" class="form_input" name="city" placeholder=" "/>
+                <label for="city">Stadt</label>
+            </div>
+        </fieldset>
+        <input id="email" type="email" class="form_input" name="email" placeholder=" "/>
+        <label for="email">E-Mail</label>
+        <input id="password" type="password" class="form_input" name="password" placeholder=" "/>
+        <label for="password">Passwort</label>
+        <input id="password2" type="password" class="form_input" name="password2" placeholder=" "/>
+        <label for="password2">Passwort wiederholen</label>
+
+        <button type="submit">Registrieren</button>
+        <a class="link-button" href="/login">Zum Log In wechseln</a>
         <?php if (isset($errorMessage)): ?>
-            <p class="errorMessage">
+            <p class="error">
                 <?= $errorMessage ?>
             </p>
         <?php endif; ?>
 
     </form>
-</div>
+</main>
+
+<?php require __DIR__ . '/../layout/footer.php'; ?>
 
 </body>
 </html>
