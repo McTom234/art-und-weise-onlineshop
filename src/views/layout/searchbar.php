2b1a94a69f7afda028b0645d1097cab183fc45ef
Jonas <jonas.bellmann@gmx.de>
2021-10-11 02:46:33 +0200

    searchbar component upgrade

--- src/views/layout/searchbar.php
+++ src/views/layout/searchbar.php
@@ -1,6 +1,8 @@
-<form>
-    <label>
-        <input type="text" name="q">
-    </label>
+<form class="searchbar">
+    <label class="label-placeholder">Suche:</label>
+    <input id="searchbar" type="text" name="q" placeholder=" "
+        <?php if (isset($_GET['q'])) { echo 'value="'.$_GET['q'].'"'; } ?>
+    >
+    <label for="searchbar">Suche:</label>
     <button type="submit">Suchen!</button>
 </form>
