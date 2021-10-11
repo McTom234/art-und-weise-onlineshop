ef3948812868d31f7bf784f134ad66661cd5d57f
Jonas <jonas.bellmann@gmx.de>
2021-10-11 03:15:31 +0200

    small fixes

--- src/views/layout/searchbar.php
+++ src/views/layout/searchbar.php
@@ -1,5 +1,4 @@
 <form class="searchbar">
-    <label class="label-placeholder">Suche:</label>
     <input id="searchbar" type="text" name="q" placeholder=" "
         <?php if (isset($_GET['q'])) { echo 'value="'.$_GET['q'].'"'; } ?>
     >
