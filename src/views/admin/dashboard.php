67787e447613cc1f9d2ec63ee293bd9b491b7831
Tom McTom <mc_tom234@gmx.de>
2021-11-21 12:19:43 +0100

    rework of admin stylesheets

--- src/views/admin/dashboard.php
+++ src/views/admin/dashboard.php
@@ -5,124 +5,122 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Schülerfirma Art und Weise</title>
-    <link rel="stylesheet" href="/assets/css/home.css">
-    <link rel="stylesheet" href="/assets/css/admin.css">
+    <link rel="stylesheet" href="/assets/css/admin/dashboard.css">
 </head>
 <body>
 <?php require __DIR__ .'/../layout/navbarAdmin.php'; ?>
-<div class="grid-container">
-    <div class="grid-column--8 grid-column">
-        <h3 class="card-title">Bestellungen</h3>
-        <table>
-            <thead>
-            <tr>
-                <th>Produkt ID</th>
-                <th>Rabatt</th>
-                <th>Anzahl</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($orders as $order): ?>
+<main>
+    <h2>Administrator-Dashboard</h2>
+    <div class="grid-container">
+        <div class="grid-column--8 grid-column">
+            <h3 class="card-title">Bestellungen</h3>
+            <table>
+                <thead>
                 <tr>
-                    <td>
-                        <a href="/show?id=<?=$order->product_ID; ?>"><?=$order->product_ID; ?></a>
-                    </td>
-                    <td>
-                        <?= $order->discount; ?>
-                    </td>
-                    <td>
-                        <?= $order->quantity; ?>
-                    </td>
+                    <th>Produkt ID</th>
+                    <th>Rabatt</th>
+                    <th>Anzahl</th>
+                    <th></th>
                 </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-        <div class="card-bottom">
-            <a href="/admin/orders">
-                <button>Gehe zur Übersicht</button>
-            </a>
+                </thead>
+                <tbody>
+                <?php foreach ($orders as $order): ?>
+                    <tr>
+                        <td><?=$order->product_ID; ?></td>
+                        <td><?= $order->discount; ?></td>
+                        <td><?= $order->quantity; ?></td>
+                        <td><a href="/show?id=<?=$order->product_ID; ?>" class="link-button">Ansehen</a></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+            <div class="card-bottom">
+                <a href="/admin/orders">
+                    <button>Gehe zur Übersicht</button>
+                </a>
+            </div>
         </div>
-    </div>
-    <div class="grid-column--4 grid-column">
-        <h3 class="card-title">Mitglieder</h3>
-        <div>
+        <div class="grid-column--4 grid-column">
+            <h3 class="card-title">Mitglieder</h3>
+            <div>
 
+            </div>
+            <div class="card-bottom">
+                <a href="">
+                    <button>Gehe zur Übersicht</button>
+                </a>
+            </div>
         </div>
-        <div class="card-bottom">
-            <a href="">
-                <button>Gehe zur Übersicht</button>
-            </a>
-        </div>
-    </div>
-    <div class="grid-column--6 grid-column">
-        <h3 class="card-title">Produkte</h3>
-        <table>
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Preis</th>
-                <th>Rabatt</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($products as $product): ?>
+        <div class="grid-column--6 grid-column">
+            <h3 class="card-title">Produkte</h3>
+            <table>
+                <thead>
                 <tr>
-                    <td>
-                        <?= $product->name; ?>
-                    </td>
-                    <td>
-                        <?= $product->price; ?>
-                        €
-                    </td>
-                    <td>
-
-                        <?php if (empty($product->discount)): ?>
-                            kein Rabatt
-                        <?php else:?>
-                            <?=$product->discount; ?>%
-                        <?php endif;?>
-                    </td>
-                    <td>
-                        <a href="">
-                            <button>Bearbeiten</button>
-                        </a>
-                    </td>
+                    <th>Name</th>
+                    <th>Preis</th>
+                    <th>Rabatt</th>
+                    <th></th>
                 </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-        <div class="card-bottom">
-            <a href="/admin/products">
-                <button>Gehe zur Übersicht</button>
-            </a>
+                </thead>
+                <tbody>
+                <?php foreach ($products as $product): ?>
+                    <tr>
+                        <td>
+                            <?= $product->name; ?>
+                        </td>
+                        <td>
+                            <?= $product->price; ?>
+                            €
+                        </td>
+                        <td>
+
+                            <?php if (empty($product->discount)): ?>
+                                kein Rabatt
+                            <?php else:?>
+                                <?=$product->discount; ?>%
+                            <?php endif;?>
+                        </td>
+                        <td>
+                            <a href="">
+                                <button>Bearbeiten</button>
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+            <div class="card-bottom">
+                <a href="/admin/products">
+                    <button>Gehe zur Übersicht</button>
+                </a>
+            </div>
+        </div>
+        <div class="grid-column--6 grid-column">
+            <h3 class="card-title">Finzanzen</h3>
+            <div class="card-bottom">
+                <a href="">
+                    <button>Gehe zur Übersicht</button>
+                </a>
+            </div>
+        </div>
+        <div class="grid-column--8 grid-column">
+            <h3 class="card-title">News</h3>
+            <div class="card-bottom">
+                <a href="">
+                    <button>Gehe zur Übersicht</button>
+                </a>
+            </div>
+        </div>
+        <div class="grid-column--4 grid-column">
+            <h3 class="card-title">Bilder</h3>
+            <div class="card-bottom">
+                <a href="">
+                    <button>Gehe zur Übersicht</button>
+                </a>
+            </div>
         </div>
     </div>
-    <div class="grid-column--6 grid-column">
-        <h3 class="card-title">Finzanzen</h3>
-        <div class="card-bottom">
-            <a href="">
-                <button>Gehe zur Übersicht</button>
-            </a>
-        </div>
-    </div>
-    <div class="grid-column--8 grid-column">
-        <h3 class="card-title">News</h3>
-        <div class="card-bottom">
-            <a href="">
-                <button>Gehe zur Übersicht</button>
-            </a>
-        </div>
-    </div>
-    <div class="grid-column--4 grid-column">
-        <h3 class="card-title">Bilder</h3>
-        <div class="card-bottom">
-            <a href="">
-                <button>Gehe zur Übersicht</button>
-            </a>
-        </div>
-    </div>
-</div>
+</main>
 <?php require '../views/layout/footer.php'; ?>
 </body>
 </html>
--- src/views/layout/navbarAdmin.php
+++ src/views/layout/navbarAdmin.php
@@ -1,17 +1,18 @@
 <?php if (!isset($navbar_index)) $navbar_index = ""; ?>
 <nav id="navbar">
     <div class="left">
-        <a class="nav-button-home" href="/" <?php if ($navbar_index == "home") echo "data-active"; ?>>
-            Art und Weise
-        </a>
+        <a class="nav-button-home" href="/" <?php if ($navbar_index == "home") echo "data-active"; ?>>Art und Weise</a>
+
         <div class="dropdown">
-            <a href="/admin/dashboard" <?php if (in_array($navbar_index, array("link 1", "link 2", "link 3"))) echo "data-active"; ?>>
-                Dashboard
-            </a>
+            <a href="/admin/dashboard" <?php if (in_array($navbar_index, array("link 1", "link 2", "link 3"))) echo "data-active"; ?>>Dashboard</a>
+
             <div class="dropdown-content">
                 <a href="/admin/orders" <?php if ($navbar_index == "link 1") echo "data-active"; ?>>Bestellungen</a>
+
                 <a href="/admin/products" <?php if ($navbar_index == "link 2") echo "data-active"; ?>>Produkte</a>
+
                 <a href="/admin/members" <?php if ($navbar_index == "link 3") echo "data-active"; ?>>Mitglieder</a>
+
                 <a href="/" <?php if ($navbar_index == "link 4") echo "data-active"; ?>>Finanzen</a>
             </div>
         </div>
@@ -20,16 +21,11 @@
     <div class="right">
         <?php if ($loggedIn): ?>
             <span class="navbar-text"> <?= $loggedIn->forename . ' ' . $loggedIn->surname ?></span>
+
             <a href="/logout">Abmelden</a>
-        <?php else: ?>
-            <a href="/login" <?php if ($navbar_index == "login") echo "data-active"; ?>>
-                Anmelden
-            </a>
-            <a href="/registration" <?php if ($navbar_index == "registration") echo "data-active"; ?>>
-                Registrieren
-            </a>
-        <?php endif; ?>
+        <?php else: die("Login required"); endif; ?>
     </div>
+
     <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
 </nav>
 
