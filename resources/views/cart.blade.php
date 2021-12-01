e229ead6b417481a2b78e55ba973cd4800723362
Tom McTom <mc_tom234@gmx.de>
2021-12-01 22:52:28 +0100

    adjust meta tags

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Schülerfirma Art und Weise | Warenkorb</title>
     <link rel="stylesheet" href="{{asset('css/shoppingCart.css')}}">
 </head>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
     <title>Schülerfirma Art und Weise</title>
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
