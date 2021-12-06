1db26bf9276e57dcc96c862df4c8a3560406d2d0
Jonas Bellmann <jonas.bellmann@ivao.aero>
2021-12-06 23:30:06 +0100

    rework cart blade with product-list blade and custom html tag

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,9 +8,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Schülerfirma Art und Weise | Warenkorb</title>
     <link rel="stylesheet" href="{{asset('css/shoppingCart.css')}}">
-
-    <script src="{{asset('js/shoppingCart.js')}}"></script>
-    <script src="{{asset('js/quantitySelect.js')}}"></script>
 </head>
 
 <body>
@@ -19,12 +16,30 @@
 <main>
     <section id="cart-list">
         <h2>Warenkorb</h2>
+        @each('layouts.product-list', $products, 'product')
     </section>
 
-    @each('layouts.cart-product', $products, 'product')
     <a href="{{route('checkout.details')}}" id="buy-button" class="link-button">Kaufen</a>
 </main>
 
 @include('layouts.footer')
+
+<script src="{{asset('js/shoppingCart.js')}}"></script>
+<script src="{{asset('js/quantitySelect.js')}}"></script>
+<script>
+    const wrappers = document.getElementsByTagName("quantity");
+    for (let i = 0; i < wrappers.length; i++) {
+        const wrapper = wrappers.item(i);
+        wrapper.append(
+            createQuantitySelect(wrapper.attributes.getNamedItem("data-number").nodeValue, [1, 2, 3, 4, 5], 100, true, function (number) {
+                {{--todo total price span with custom tag and data- attributes--}}
+                {{--let price = document.getElementById('total-price');--}}
+                {{--price.textContent = ({{$product->getDiscountPriceEuro()}} * number).toFixed(2).toString().replace(".", ",");--}}
+                // todo add shopping cart button as custom tag and modify value
+                setItem(wrapper.attributes.getNamedItem("data-id").nodeValue, number, 0)
+            })
+        );
+    }
+</script>
 </body>
 </html>
--- resources/views/layouts/product-list.blade.php
+++ resources/views/layouts/product-list.blade.php
@@ -1,26 +1,25 @@
 @php
-    // $need_figure = $product->images()->first()
-    $need_figure = true;
+    $need_figure = $product->images()->first()
 @endphp
 <article class="{{$need_figure ? '' : 'no-figure'}}">
     @if($need_figure)
         <figure>
-            <a>
-                <img/>
+            <a class="no-text-decoration" href="{{url('/product', $product->id)}}">
+                <img src="{{$product->images()->first()->base64}}" alt="{{$product->name}}"/>
             </a>
         </figure>
     @endif
     <h3>
-        <a></a>
+        <a class="no-text-decoration" href="{{url('/product', $product->id)}}">{{$product->name}}</a>
     </h3>
     <p>
-        <a></a>
+        <a class="no-text-decoration" href="{{url('/product', $product->id)}}">{{$product->description}}</a>
     </p>
     <div>
         <div class="item-control">
-            <div id="quantitySelect-wrapper"></div>
-            <button></button>
+            <quantity data-id="{{$product->id}}" data-number="{{$product->number}}"></quantity>
+            <button onclick="(async function(){ await deleteItem({{$product->id}}); window.location.reload(true); }) ();">Löschen</button>
         </div>
-        <span></span>
+        <span data-id="{{$product->id}}">{{number_format($product->getDiscountPriceEuro()*$product->number, 2, ',', '.')}}</span>
     </div>
 </article>
