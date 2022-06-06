e66fee9bfd7179ab1f5e22c8eabf57510e422c8c
McTom234 <mc_tom234@gmx.de>
2022-06-06 11:06:09 +0200

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,6 +21,14 @@
 
     <p>Wir arbeiten zur Zeit mit Hochdruck an der Fertigstellung dieser Seite.<br/>
         Sollten Sie sonstige Fragen zu der Schülerfirma <b>Art und Weise</b>, dem Konzept von Schülerfirmen oder generelles Interesse haben und gerne mehr erfahren möchten, können Sie uns per <a href="mailto:jonas">E-Mail</a> erreichen.</p>
+
+    <h3>Ablauf der Bestellung</h3>
+
+    <p></p>
+
+    <h3>Versand und Versandkosten</h3>
+
+    <p></p>
 </main>
 
 @include('layouts.footer')
